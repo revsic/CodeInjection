8843fff259642ed7428d7f6c00b350b542244ec2
revsic <revsic99@gmail.com>
2017-03-17 17:05:14 +0900

    Make injector wait for thread object

--- injectDll.cpp
+++ injectDll.cpp
@@ -1,75 +1,62 @@
-#include <iostream>
-#include <wchar.h>
 #include <Windows.h>
-#include <TlHelp32.h>
+#include <stdio.h>
 
-#define TARGET L"test.exe"
-#define DLL_PATH L"Examples.dll"
+#define MAX_FILE_PATH 512
 
-typedef struct Param {
-	FARPROC loadLibrary;
-	WCHAR dllName[1024];
-} PARAM;
+typedef HMODULE(WINAPI *PLOADLIBRARYW) (LPCWSTR lpLibFileName);
 
-typedef HMODULE(__stdcall *PLOADLIBRARYW) (
-	LPCWSTR lpLibFileName
-	);
+typedef struct {
+	PLOADLIBRARYW lpLoadLibraryW;
+	WCHAR lpLibFileName[MAX_FILE_PATH];
+} PARAM, *PPARAM;
 
-void InjectFunction(LPVOID lpParam) {
-	PARAM* param = (PARAM *)lpParam;
-	HMODULE hmd = ((PLOADLIBRARYW)param->loadLibrary)(param->dllName);
-}
-
-DWORD GetPidByProcessName(WCHAR *name);
-
-int main() {
-	SIZE_T written;
-	int dataSize = -1;
-	unsigned char* data = (unsigned char *)InjectFunction;
-	while (data[++dataSize] != 0xc3);
-
-	DWORD pid = GetPidByProcessName(TARGET);
-	std::cout << "[*] pid : " << pid << std::endl;
-
-	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-
-	PARAM param;
-	HMODULE hMod = LoadLibraryW(L"Kernel32.dll");
-	param.loadLibrary = GetProcAddress(hMod, "LoadLibraryW");
-	wcscpy(param.dllName, DLL_PATH);
-
-	LPVOID vAddr = VirtualAllocEx(hProcess, NULL, dataSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	LPVOID argAddr = VirtualAllocEx(hProcess, NULL, sizeof(PARAM), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-
-	std::cout << "[*] vAddr " << std::hex << vAddr << std::endl;
-	std::cout << "[*] argAddr " << argAddr << std::endl;
-
-	WriteProcessMemory(hProcess, vAddr, data, dataSize + 1, &written);
-	WriteProcessMemory(hProcess, argAddr, (unsigned char *)&param, sizeof(param), &written);
-
-	system("pause");
-	CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)vAddr, argAddr, 0, NULL);
-	
+int InjectFunction(PPARAM param) {
+	(param->lpLoadLibraryW)(param->lpLibFileName);
 	return 0;
 }
 
-DWORD GetPidByProcessName(WCHAR *name) {
-	PROCESSENTRY32W entry;
-	memset(&entry, 0, sizeof(PROCESSENTRY32W));
-	entry.dwSize = sizeof(PROCESSENTRY32W);
-	
-	DWORD pid = -1;
-	HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-	if (Process32FirstW(hSnapShot, &entry)) {
-		do {
-			if (!wcscmp(name, entry.szExeFile)) {
-				pid = entry.th32ProcessID;
-				break;
-			}
-		} while (Process32Next(hSnapShot, &entry));
-	}
+int EndOfFunction() { return 0; }
 
-	CloseHandle(hSnapShot);
+int main(int argc, char *argv[]) {
+	WCHAR *target = L"target.exe";
+	WCHAR *lib = L"lib.dll";
 	
-	return pid;
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	memset(&si, 0, sizeof(si));
+	memset(&pi, 0, sizeof(pi));
+
+	CreateProcessW(target, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+
+	HMODULE hKernel32 = LoadLibraryW(L"kernel32.dll");
+
+	PARAM param;
+	wcscpy(param.lpLibFileName, lib);
+	param.lpLoadLibraryW = (PLOADLIBRARYW)GetProcAddress(hKernel32, "LoadLibraryW");
+
+	DWORD64 dwFunctionSize = (DWORD64)EndOfFunction - (DWORD64)InjectFunction;
+
+	LPVOID lpFunction = VirtualAllocEx(pi.hProcess, NULL, dwFunctionSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	LPVOID lpParam = VirtualAllocEx(pi.hProcess, NULL, sizeof(param), MEM_COMMIT, PAGE_READWRITE);
+
+	SIZE_T written;
+	WriteProcessMemory(pi.hProcess, lpParam, &param, sizeof(param), &written);
+	WriteProcessMemory(pi.hProcess, lpFunction, InjectFunction, dwFunctionSize, &written);
+
+	HANDLE hThread = CreateRemoteThread(pi.hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)lpFunction, lpParam, NULL, NULL);
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+	ResumeThread(pi.hThread);
+
+	VirtualFreeEx(pi.hProcess, lpFunction, dwFunctionSize, MEM_RELEASE);
+	VirtualFreeEx(pi.hProcess, lpParam, sizeof(param), MEM_RELEASE);
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+
+	return 0;
 }
